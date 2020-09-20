f665f88513fd6d5f470c2e21f846698c02fd3217
codedlord <63451785+codedlord@users.noreply.github.com>
2020-09-20 16:11:09 -0700

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,9 @@
+<html>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="materialize/css/materialize.min.css">
+<body>
+<?php include('template/header.php'); ?>
+	<?php include('template/footer.php'); ?>	
+</body>
+</html>
